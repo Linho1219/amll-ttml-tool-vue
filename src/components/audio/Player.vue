d3f2ce4f750f43da290303f09c930720efac1c5d
Linho <linho1219@outlook.com>
2025-10-30 15:06:44 +0800

    音频组件初步

--- src/components/audio/Player.vue
+++ src/components/audio/Player.vue
@@ -1,19 +1,44 @@
 <template>
   <Card class="player">
     <template #content>
-      <Button icon="pi pi-upload" severity="secondary" /><Button
-        icon="pi pi-sliders-v"
-        severity="secondary"
+      <Button icon="pi pi-upload" severity="secondary" @click="() => handleSelectFile()" />
+      <Button icon="pi pi-sliders-v" severity="secondary" @click="tooglePopover" />
+      <Popover ref="popover"> </Popover>
+      <Button
+        :icon="playingRef ? 'pi pi-pause' : 'pi pi-play'"
+        @click="playingRef = !playingRef"
+        :disabled="!activatedRef"
       />
-      <Button icon="pi pi-caret-right" />
-      <div style="flex: 1"></div>
+      <div style="flex: 1">
+        <PopoverPane :audio="audio" />
+      </div>
       <Button icon="pi pi-chart-bar" severity="secondary" />
     </template>
   </Card>
 </template>
 
 <script setup lang="ts">
-import { Button, Card } from 'primevue'
+import { useAudioCtrl } from '@/utils/audio'
+import { useFileDialog } from '@vueuse/core'
+import { Button, Card, InputGroup, InputGroupAddon, InputNumber, Popover } from 'primevue'
+import { useTemplateRef } from 'vue'
+import PopoverPane from './Popover.vue'
+
+const audio = useAudioCtrl()
+const { progressRef, playingRef, volumeRef, activatedRef } = audio
+const { open: handleSelectFile, onChange: onFileChange } = useFileDialog({
+  accept: 'audio/*',
+  multiple: false,
+})
+
+onFileChange((files) => {
+  const file = files?.[0]
+  if (!file) return
+  audio.mount(file)
+})
+
+const popover = useTemplateRef('popover')
+const tooglePopover = (e: MouseEvent) => popover.value?.toggle(e)
 </script>
 
 <style lang="scss">
---
+++ src/components/audio/Popover.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="audio-popover-pane">
+    <span class="audio-popover-label">音量</span>
+    <InputNumber class="monospace" :min="0" size="small" placeholder="0" />
+    <Button
+      class="audio-popover-reset-button"
+      icon="pi pi-refresh"
+      severity="secondary"
+      variant="text"
+      size="small"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { AudioCtrl } from '@/utils/audio'
+import { Button, InputNumber } from 'primevue';
+
+const props = defineProps<{ audio: AudioCtrl }>()
+</script>
+
+<style lang="scss">
+.audio-popover-pane {
+  // test
+  position: fixed;
+  width: 15rem;
+  bottom: 5rem;
+  left: 5rem;
+
+  display: grid;
+}
+</style>
