568dee74747c795634c61fc7272c4464507505ed
Linho <lhp@linho.cc>
2025-11-02 00:24:37 +0800

    时轴页面框架

--- src/components/editor/timing/TimingEditor.vue
+++ src/components/editor/timing/TimingEditor.vue
@@ -1,11 +1,26 @@
 <template>
-  <div class="editor-shell timing">
-    <div class="editor timing"></div>
+  <div class="editor timing">
+    <RecycleScroller
+      class="editor-scroller"
+      :items="coreStore.lyricLines"
+      :item-size="rem(5)"
+      v-slot="{ item: line, index: lineIndex }"
+    >
+      {{ line.words.map((words) => words.word).join('') }}
+    </RecycleScroller>
   </div>
 </template>
 
+<script setup lang="ts">
+import { useCoreStore } from '@/stores/core'
+import { RecycleScroller } from 'vue-virtual-scroller'
+
+const coreStore = useCoreStore()
+const rem = (v: number) => v * 14
+</script>
+
 <style lang="scss">
-.editor-shell.timing {
+.editor.timing {
   flex: 1;
   overflow-y: auto;
   overflow-x: hidden;
